8a11c1f10973ab96be500ad65abb0b1315bb4679
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-27 18:24:38 +0200

    working form

--- script.js
+++ script.js
@@ -16,67 +16,81 @@ Generiamo gli input da JS, invece di scriverli nel codice
 // * Recupero gli elementi dal DOM
 const givenNumbers = document.getElementById('givennumbers');
 const resultElement = document.getElementById('result');
-const inputs = document.querySelectorAll('input[type = "number"]');
+const inputs = document.querySelectorAll('input');
 const form = document.querySelector('form');
 
 
 // * Creo un array dove mettere i numeri casuali
 const randomNumbers = [];
-// * Array con i numeri corretti
-const correctNumbers = [];
 // * Array dove arriveranno i numeri dell'utente
 const userNumbers = [];
+// * Array con i numeri corretti
+const correctNumbers = [];
 
 // @ FUNCTIONS 
 
-// * Creo 5 numeri casuali con una funzione
+// * Funzione per creare numeri casuali
 function getRandomNumbers(min = 1, max = 100, isMaxIncluded = true) {
     if (isMaxIncluded) max++;
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// funzione per inserire i numeri dell'utente dal form a JS
+// * Funzione per inserire i numeri dell'utente dal form a JS
+function getUserNumbers() {
 
-function getUserNumbers(event) {
-    event.preventDefault();
+    for (let i = 0; i < inputs.length; i++) {
+        userNumbers.push(parseInt(inputs[i].value));
+    }
 
-    userNumbers.push(inputs);
     console.log('usernumbers: ', userNumbers)
 }
 
 
 // random numbers
-// * Li inserisco all'interno dell'array
+
+// * Creo 5 numeri random
+
 for (i = 0; i < 5; i++) {
     const fiveNumbers = getRandomNumbers();
+
+    // * Li inserisco all'interno dell'array
     randomNumbers.push(fiveNumbers);
 }
 console.log(randomNumbers);
 
-// * Li inserisco in pagina
+// * Li stampo in pagina
 givenNumbers.innerHTML = randomNumbers;
+
 
 // # DATA GATHERING
 
-form.addEventListener('submit', getUserNumbers);
 
+form.addEventListener('submit', function (event) {
 
-// "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"
+    // ! Blocco il form
+    event.preventDefault();
 
-// * Confrontare numeri in comune
+    // * Prendo i numeri dati dall'utente
+    getUserNumbers();
 
-for (let i = 0; i < userNumbers.length; i++) {
-    let userNumber = userNumbers[i];
-    // Se nel mio array c'è il numero dell'utente, mettilo nell'array dei corretti
-    if (randomNumbers.includes(userNumber)) {
-        correctNumbers.push(userNumber);
+    // * Li confronto con i numeri casuali
+
+    for (let i = 0; i < userNumbers.length; i++) {
+        let userNumber = userNumbers[i];
+        // Se nel mio array c'è il numero dell'utente, mettilo nell'array dei corretti
+        if (randomNumbers.includes(userNumber)) {
+            correctNumbers.push(userNumber);
+        }
     }
-}
 
-// * Creo il messaggio che apparirà a fine gioco.
-let message = `Hai memorizzato correttamente ${correctNumbers.length} numero/i! (${correctNumbers})`
+    // * Stampo in pagina il risultato
 
-resultElement.innerHTML = message;
+    let message = `Hai memorizzato correttamente ${correctNumbers.length} numero/i! (${correctNumbers})`
+
+    resultElement.innerHTML = message;
+
+});
+
 
 
 
